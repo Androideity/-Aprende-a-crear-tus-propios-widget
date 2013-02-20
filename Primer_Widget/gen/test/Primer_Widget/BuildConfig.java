e8e29a0002171a8a51060dc0bc095e2e6e2c9aa8
sebastian <sebastian@julieta>
2013-02-20 11:01:57 -0300

    .

---
+++ Primer_Widget/gen/test/Primer_Widget/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package test.Primer_Widget;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Primer_Widget/src/test/Primer_Widget/Miwidget.java
@@ -0,0 +1,15 @@
+package test.Primer_Widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class Miwidget extends AppWidgetProvider{
+	
+	 @Override
+	    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+	                 int[] appWidgetIds) {
+	       
+	    }
+
+}
